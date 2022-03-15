c0aeb5a9b46e25c2a1dae698649ce990c42582b1
Duncan Mathenge <muriithiduncan963@gmail.com>
2022-03-15 15:12:30 +0300

    Create 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - check your code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	      char p = 'a';
+
+	      while (p <= 'z')
+        {
+		          putchar (p);
+              p++;
+        }
+	      putchar ('\n');
+}
