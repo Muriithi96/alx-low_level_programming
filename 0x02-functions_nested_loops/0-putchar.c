7fde72d494207b46042ffc66167bf07d3ad7793f
Duncan Mathenge <muriithiduncan963@gmail.com>
2022-03-15 12:07:14 +0300

    Latest commit file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "school.h"
+/**
+ * main - Prints School
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_putchar('S');
+	_putchar('c');
+	_putchar('h');
+	_putchar('o');
+	_putchar('o');
+	_putchar('l');
+	_putchar('\n');
+	return (0);
+}
