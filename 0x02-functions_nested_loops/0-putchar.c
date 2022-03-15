d4696e8b5937902d49376575797106b836d0c0b5
Duncan Mathenge <muriithiduncan963@gmail.com>
2022-03-15 16:07:40 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "stdio.h"
 /**
  * main - Prints "_putchar" followed by a new line.
- * 
+ *
  * Return: Always 0 (Success)
  *
  */
@@ -11,10 +11,10 @@ int main(void)
 	int i;
 	char P[] = "_Putchar";
 
-	         for (i = 0; i < 9; i++)
-		 {
+	        for (i = 0; i < 9; i++)
+		{
 			 putchar(P[i]);
-		 }
+		}
 	putchar('\n');
-        return (0);
+return (0);
 }
