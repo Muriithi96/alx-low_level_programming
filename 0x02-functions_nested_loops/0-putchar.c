6eee411c4cfbb8e6fcdfb1d4495c16b0a3421c72
Duncan Mathenge <muriithiduncan963@gmail.com>
2022-03-15 16:01:58 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,14 +6,16 @@
  * Return: Always 0 (Success)
  *
  */
+
 int main(void)
 {
 	int i;
 	char P[] = "_Putchar";
+
 	         for (i = 0; i < 9; i++)
 		 {
-			 _putchar(P[i]);
+			 putchar(P[i]);
 		 }
-	_putchar('\n');
+	putchar('\n');
         return (0);
 }
