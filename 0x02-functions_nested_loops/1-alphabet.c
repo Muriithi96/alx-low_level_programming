5c31c3eef91a16ceb47961b90aef9d4fa86a303d
Duncan Mathenge <muriithiduncan963@gmail.com>
2022-03-16 08:11:41 +0300

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,17 @@
-#​include​ ​"​main.h​" 
- 
-/*​* 
- ​* print_alphabet -prints out alphabet 
- ​* 
- ​* Return: void 
- ​*/ 
-  
-​void​ ​print_alphabet​(​void​) 
-  
-​{ 
-​char​ x; 
-for​ (x = ​'​a​'​; x <= ​'​z​'​; x++) 
-_putchar​(x); 
-  
-_putchar​(​'​\n​'​); 
+#include "main.h"
+
+/**
+ * print_alphabet -prints out alphabet
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+
+{
+char x;
+for (x = 'a'; x <= 'z'; x++)
+_putchar(x);
+
+_putchar('\n');
 }
